2a5158aa67bdd4f50624d99f08d55d50f1361711
lely <lely@thoughtmachine.net>
2019-07-23 17:49:29 +0100

    Slight change to parsing service to avoid storing a null value in service body

--- parser/service.go
+++ parser/service.go
@@ -185,6 +185,7 @@ func (p *Parser) parseServiceBody() (
 			if err != nil {
 				return nil, nil, scanner.Position{}, err
 			}
+			continue
 		}
 
 		p.MaybeScanInlineComment(stmt)
@@ -237,7 +238,7 @@ func (p *Parser) parseRPC() (*RPC, error) {
 	default:
 		return nil, p.unexpected("{ or ;")
 	}
-
+	//p.parseTrailingSemis()
 	return &RPC{
 		RPCName:     rpcName,
 		RPCRequest:  rpcRequest,
@@ -245,6 +246,15 @@ func (p *Parser) parseRPC() (*RPC, error) {
 		Options:     opts,
 		Meta:        meta.NewMetaWithLastPos(startPos, p.lex.Pos),
 	}, nil
+}
+
+func (p *Parser) parseTrailingSemis() {
+	for {
+		if p.lex.Peek() != scanner.TSEMICOLON {
+			break
+		}
+		p.lex.Next()
+	}
 }
 
 // rpcRequest = "(" [ "stream" ] messageType ")"
